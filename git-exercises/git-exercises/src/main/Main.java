24f9365a06b06795360cdc453d160c33d7115746
MB105 <124814937+MB105@users.noreply.github.com>
2024-02-01 11:58:52 +0100

    changed from 100 to 200

--- git-exercises/git-exercises/src/main/Main.java
+++ git-exercises/git-exercises/src/main/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 200; i++) {
             if(i%3==0) {
                 System.out.println("Fizz");
             }else if(i%5==0) {
